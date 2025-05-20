00a63f1a64b1b99e5d1b71b027c561844b370d1a
Joao Gomes <jg0112360@gmail.com>
2025-05-20 18:34:52 -0300

    feat: base code

---
+++ src/main/java/com/project/hourbase/HourbaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.hourbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourbaseApplication.class, args);
+	}
+
+}
