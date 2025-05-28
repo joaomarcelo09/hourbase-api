7efac3ce04a2c83034fd772139b22ee318c19cca
Joao Gomes <jg0112360@gmail.com>
2025-05-27 22:24:06 -0300

    feat: company and service of company

---
+++ src/main/java/com/project/hourbase/company/CompanyRepository.java
@@ -0,0 +1,6 @@
+package com.project.hourbase.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, String> {
+}
---
+++ src/main/java/com/project/hourbase/company/dto/CreateCompanyReq.java
@@ -0,0 +1,4 @@
+package com.project.hourbase.company.dto;
+
+public record CreateCompanyReq(String name, Integer contracted_hours) {
+}
