701deaa84cc0c7c5aef129a816f60ccadedd3d1d
Joao Gomes <jg0112360@gmail.com>
2025-06-02 17:57:06 -0300

    feat: new update, delete and gets services

--- src/main/java/com/project/hourbase/company/CompanyService.java
+++ src/main/java/com/project/hourbase/company/CompanyService.java
@@ -1,6 +1,8 @@
 package com.project.hourbase.company;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.project.hourbase.company.dto.CreateCompanyReq;
@@ -17,7 +19,38 @@ public class CompanyService {
 
 		Company newCompany = new Company();
 		newCompany.setName(data.name());
-		newCompany.setContractedHours(data.contracted_hours());
+		newCompany.setContracted_hours(data.contracted_hours());
 
+		companyRepository.save(newCompany);
+
+	}
+
+	public Optional<Company> findCompany(String id_company) {
+
+		Optional<Company> company = companyRepository.findById(id_company);
+
+		return company;
+	}
+
+	public List<Company> findAllCompanies() {
+
+		List<Company> companies = companyRepository.findAll();
+
+		return companies;
+	}
+
+	public void deleteCompany(String id_company) throws Exception {
+
+		Company deleteCompany = findCompany(id_company).orElseThrow();
+
+		companyRepository.delete(deleteCompany);
+
+	}
+
+	public void updateCompany(String id_company, CreateCompanyReq data) {
+		Company company = findCompany(id_company).orElseThrow();
+
+		company.setName(data.name());
+		company.setContracted_hours(data.contracted_hours());
 	}
 }
